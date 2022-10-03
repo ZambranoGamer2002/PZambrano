6a40b975cc040e208b226348192689eed7f1db22
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-10-03 17:42:21 -0500

    Examen

---
+++ src/main/java/com/zambrano/demo/PzambranoApplication.java
@@ -0,0 +1,13 @@
+package com.zambrano.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PzambranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PzambranoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zambrano/demo/PzambranoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zambrano.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PzambranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
