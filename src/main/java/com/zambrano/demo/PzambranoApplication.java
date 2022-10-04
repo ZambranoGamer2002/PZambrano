00ad7b7f922bd48975f8775930b8414b533243d4
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-10-03 19:36:50 -0500

    Examen

--- src/main/java/com/zambrano/demo/PzambranoApplication.java
+++ src/main/java/com/zambrano/demo/PzambranoApplication.java
@@ -19,6 +19,6 @@ public class PzambranoApplication {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.upia.RUpiachihua")).build();
+                .apis(RequestHandlerSelectors.basePackage("com.zambrano.demo")).build();
     }
 }
